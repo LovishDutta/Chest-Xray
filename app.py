2911c444923a488403cfeec63e67fa23b1f96a56
LovishDutta <lovishdutta30@gmail.com>
2026-03-12 09:49:15 +0530

    app change

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import numpy as np
 import cv2
-from tensorflow.keras.models import load_model
+from keras.models import load_model
 from gradcam import make_gradcam_heatmap, overlay_heatmap
 
 # Load model
